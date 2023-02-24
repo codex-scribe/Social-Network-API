93fb702efdab195c9395e62229a05d79459a1f3c
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-23 23:33:20 -0500

    more routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
+//localhost:3001/api/ -->
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+//link functions here
+
+router.route('/');
+
+module.exports = router;
